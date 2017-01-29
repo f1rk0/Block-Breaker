e15798e5968a635a14c4ddb65f9fa2ffe175d950
f1rk0 <f1rk0top1@gmail.com>
2017-01-29 17:13:29 +0300

    Halfway develop

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+	
+	static Music instance = null;
+	
+	void Awake (){
+
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
