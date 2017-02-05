d2bb8ae5c25e99d169fca5abcae79768200649e9
f1rk0 <f1rk0top1@gmail.com>
2017-02-05 18:36:18 +0300

    .exe is on air! B.Breaker folder contains it. Level switching added

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 
 	public int maxHits;
+	public static int brickCount = 0;
 	public int hitBrick;
 	public Sprite[] hitSprites;
+	private bool isBreakable;
 	
 	private LevelManager levelManager;
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		//Keep track of breakable bricks
+		if (isBreakable) {
+			brickCount++;
+		}
 		levelManager = GameObject.FindObjectOfType<LevelManager>();	
 	}
-	void OnCollisionEnter2D (Collision2D collision){				
-		bool isBreakable = (this.tag == "Breakable");
+	void OnCollisionEnter2D (Collision2D collision){					
 		if (isBreakable) {
 			HandleHits();
 		}
 	}
 	
 	void HandleHits(){
-		if (--maxHits <= 0 ) {Destroy(gameObject);}
+		if (--maxHits <= 0 ) {
+			brickCount--; 
+			levelManager.BrickDestroyed();
+			Destroy(gameObject);
+		}
 		else {LoadSprites ();}
 	}
 	
