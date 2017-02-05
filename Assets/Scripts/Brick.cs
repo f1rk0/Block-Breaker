0fa443512e31b01cf4b3564eda530c3728790c97
f1rk0 <f1rk0top1@gmail.com>
2017-02-05 17:29:16 +0300

    Improving assets, giving possibility of unbreakable bricks. Now changing sprites on hit. .exe soon!

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,13 +4,29 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 
 	public int maxHits;
+	public int hitBrick;
+	public Sprite[] hitSprites;
+	
 	private LevelManager levelManager;
 	// Use this for initialization
 	void Start () {
 		levelManager = GameObject.FindObjectOfType<LevelManager>();	
 	}
 	void OnCollisionEnter2D (Collision2D collision){				
+		bool isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			HandleHits();
+		}
+	}
+	
+	void HandleHits(){
 		if (--maxHits <= 0 ) {Destroy(gameObject);}
+		else {LoadSprites ();}
+	}
+	
+	void LoadSprites (){
+		int spriteIndex = hitBrick++;
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
 	}
 	
 	// TODO Remove method once bricks will be destructable
